0e90fe8a870a36e7981be453cc46da69d05c316f
Florian <info@petrockblock.com>
2014-06-17 21:44:51 +0200

    Added several emulators

--- retropackages.sh
+++ retropackages.sh
@@ -8,8 +8,10 @@ __build=()
 __install=()
 __configure=()
 __package=()
+__doPackages=0
 
 rootdir="/opt/retropie"
+romroot="~/RetroPie/roms"
 
 function getScriptAbsoluteDir() {
     # @description used to get the script path
@@ -133,20 +135,42 @@ import "scriptmodules/emulators"
 import "scriptmodules/libretrocores"
 import "scriptmodules/supplementary"
 
+# ==========================================================================
+# ==========================================================================
+
 # register script functions
 # rp_registerFunction "" "" "" "" "" "" ""
 
 # Emulators
-rp_registerFunction "100" "RetroArch Emulator             " "sources_retroarch"       "build_retroarch"         "install_retroarch"         "configure_retroarch"       ""
+rp_registerFunction "100" "RetroArch                      " "sources_retroarch"       "build_retroarch"         "install_retroarch"         "configure_retroarch"       ""
+rp_registerFunction "101" "AdvMame                        " "sources_advmame"         "build_advmame"           "install_advmame"           "configure_advmame"         ""
+rp_registerFunction "102" "Amiga emulator UAE4All         " "sources_uae4all"         "build_uae4all"           "install_uae4all"           "configure_uae4all"         ""
+rp_registerFunction "103" "Atari 800 emulator             " "sources_atari800"        "build_atari800"          "install_atari800"          "configure_atari800"        ""
+rp_registerFunction "104" "Armstrad CPC emulator          " "sources_cpc"             "build_cpc"               "install_cpc"               "configure_cpc"             ""
+rp_registerFunction "105" "DOS Emulator Dosbox            " ""                        ""                        "install_dosbox"            "configure_dosbox"          ""
+rp_registerFunction "106" "Atari2600 emulator STELLA      " ""                        ""                        "install_stella"            "configure_stella"          ""
+rp_registerFunction "107" "Macintosh emulator             " "sources_basilisk"        "build_basilisk"          "install_basilisk"          "configure_basilisk"        ""
+rp_registerFunction "108" "C64 emulator VICE              " "sources_vice"            "build_vice"              "install_vice"              "configure_vice"            ""
+rp_registerFunction "108" "C64 ROMs                       " ""                        ""                        "install_c64roms"           ""                          ""
+rp_registerFunction "109" "Duke3D Port                    " ""                        ""                        "install_eduke32"           ""                          ""
+rp_registerFunction "110" "GameBoy Advance emulator       " "sources_gpsp"            "build_gpsp"              "install_gpsp"              "configure_gpsp"            ""
+rp_registerFunction "111" "NeoGeo emulator GnGeoPi        " "sources_gngeopi"         "build_gngeopi"           "install_gngeopi"           "configure_gngeopi"         ""
+rp_registerFunction "112" "Atari emulator Hatari          " ""                        ""                        "install_hatari"           ""                          ""
 
 # LibretroCores functions
 rp_registerFunction "200" "SNES LibretroCore PocketSNES   " "sources_pocketsnes"       "build_pocketsnes"       "install_pocketsnes"        "configure_pocketsnes"      ""
 rp_registerFunction "201" "Genesis LibretroCore Picodrive " "sources_picodrive"        "build_picodrive"        "install_picodrive"         "configure_picodrive"       ""
 
 # Supplementary functions
-rp_registerFunction "300" "Package Repository             " ""                         ""                       "install_PackageRepository" "" ""
-rp_registerFunction "301" "SDL 2.0.1                      " "sources_sdl"              "build_sdl"              "install_sdl"               "" ""
-rp_registerFunction "302" "EmulationStation               " "sources_EmulationStation" "build_EmulationStation" "install_EmulationStation"  "configure_EmulationStation" "package_EmulationStation"
+rp_registerFunction "300" "Update APT packages            " ""                         ""                       "install_APTPackages" "" ""
+rp_registerFunction "301" "Package Repository             " ""                         ""                       "install_PackageRepository" "" ""
+rp_registerFunction "302" "SDL 2.0.1                      " "sources_sdl"              "build_sdl"              "install_sdl"               "" ""
+rp_registerFunction "303" "EmulationStation               " "sources_EmulationStation" "build_EmulationStation" "install_EmulationStation"  "configure_EmulationStation" "package_EmulationStation"
+rp_registerFunction "304" "EmulationStation Theme Simple  " ""                         ""                       "install_ESThemeSimple"     "" ""
+
+# ==========================================================================
+# ==========================================================================
+
 
 # ID mode
 if [[ $# -eq 1 ]]; then
